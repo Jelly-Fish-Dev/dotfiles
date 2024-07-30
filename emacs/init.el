c8b35b69a5303285f2a90a277b53a1da1250f40b
Jelly-Fish-Dev <isaac.g.hawkins@gmail.com>
2024-07-31 07:18:41 +1000

    Updated dotfiles to what I currently use, will add screenshots soon

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; -*-
+
 ;; Set up package.el to work with MELPA
 (require 'package)
 (add-to-list 'package-archives
@@ -7,24 +9,29 @@
 
 ;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'github-dark-vscode t)
+(load-theme 'warm-night :no-confirm)
 
-; disable the bars
+(electric-indent-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (fringe-mode -1)
-; internal border
+;line number
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
 ;(internal-border-width . 10)
 ; opacity
 (set-frame-parameter (selected-frame) 'alpha '(90 . 70))
 (add-to-list 'default-frame-alist '(alpha . (90 . 70)))
 ;;Font
-(set-frame-font "JetBrainsMono 13" nil t)
+(set-frame-font "JetBrainsMono 11" nil t)
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
 
+(setq evil-want-keybinding nil)
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
@@ -33,13 +40,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(org-inline-anim org-download org-journal evil)))
+ '(custom-safe-themes
+   '("9ab46512b8b69478b057f79d0eb0faee61d19e53917a868a14e41bf357cee6d4" default))
+ '(org-agenda-files '("~/Documents/Budget/June.2024.org"))
+ '(package-selected-packages
+   '(xah-fly-keys lsp-mode yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(line-number ((t (:inherit (shadow default) :background "#181515"))))
+ '(shadow ((t (:foreground "gainsboro")))))
 
 ;;; OrgMode
 ;flyspell
@@ -54,3 +66,59 @@
 (add-hook 'org-journal-hook
 	  (lambda ()
 	    (setq org-download-image-dir "~/journal/%Y.%M.%d")))
+
+;; Company
+(add-hook 'after-init-hook 'global-company-mode)
+
+(setq package-selected-packages '(lsp-mode yasnippet lsp-treemacs helm-lsp
+    projectile hydra flycheck company avy which-key helm-xref dap-mode))
+
+(when (cl-find-if-not #'package-installed-p package-selected-packages)
+  (package-refresh-contents)
+  (mapc #'package-install package-selected-packages))
+
+;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
+(helm-mode)
+(require 'helm-xref)
+(define-key global-map [remap find-file] #'helm-find-files)
+(define-key global-map [remap execute-extended-command] #'helm-M-x)
+(define-key global-map [remap switch-to-buffer] #'helm-mini)
+
+
+;;Show line numbers how I like
+(defun display-relative-line-numbers ()
+    (interactive)
+    (display-line-numbers-mode)
+    (setq display-line-numbers ' relative))
+
+(which-key-mode)
+(defun add-hook-c ()
+  (lsp)
+  (display-relative-line-numbers))
+(add-hook 'c-mode-hook #'add-hook-c)
+(add-hook 'c++-mode-hook 'lsp)
+(add-hook 'c++-mode-hook 'display-line-numbers-mode)
+(add-hook 'emacs-lisp-mode-hook 'display-relative-line-numbers)
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+(require 'compile) ; makes sure you have compile
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (require 'dap-cpptools)
+  (yas-global-mode))
+
+
+;; evil in other modes
+(require 'evil-collection)
+(with-eval-after-load 'ansi-term (evil-collection-term-setup))
+(put 'erase-buffer 'disabled nil)
+
+(add-to-list 'load-path "~/.emacs.d/predictive/")
+(add-hook 'org-journal-mode-hook '(load-library predictive))
+(add-hook 'org-journal-mode-hook 'visual-line-mode)
